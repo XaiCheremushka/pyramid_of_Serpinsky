df8b54c4916397edd27ad50807366e69ae3ddf73
XaiCheremushka <vania55ru@mail.ru>
2023-05-13 17:51:40 +0600

    fourth commit

--- Serpinski/Program.cs
+++ Serpinski/Program.cs
@@ -7,7 +7,10 @@ namespace Serpinski
 
         static void Main(string[] args)
         {
+            // Изменяем язык в программе на английский, чтобы дробная часть была отделена точкой, а не запятой 
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
 
+            // Координаты вершин
             double[][] pozition = new double[5][]; // Создаем массив массивов
 
             pozition[0] = new double[] { -1.0, -1.0, 1.0 }; // A
@@ -15,26 +18,16 @@ namespace Serpinski
             pozition[2] = new double[] { 1.0, -1.0, -1.0 }; // C
             pozition[3] = new double[] { -1.0, -1.0, -1.0 }; // D
             pozition[4] = new double[] { 0.0, 1.0, 0.0 };  // S
-            // Координаты вершин
 
-            int size = 6; // количество итераций 
-            int vertice = 0; // Количество вершин 
-            // Количество вершин в этой программе
-            vertice = (int)Math.Pow(5, size + 1);
 
-            //Фактическое количество вершин 
-            //if (size != 0)
-            //    vertice = 5 + 9 + 9 * (int)Math.Pow(5, size - 1);
-            //else vertice = 5;
-
+            Console.Write("Введите количество итераций: ");
+            int size = Convert.ToInt32(Console.ReadLine()); // количество итераций 
+            int vertice = (int)Math.Pow(5, size + 1); // Количество вершин
             int faces = (int)Math.Pow(5, size + 1); // Количество закрашенных полей
-            int edges = 8 * (int)Math.Pow(5, size + 1);
-
+            int edges = 8 * (int)Math.Pow(5, size + 1); // Количество ребер
+            string path = @"F:\vano\Учеба\Универ\Prog\Serpinski\Serpinski\Serp" + size + ".off";
 
             Faces face = new();
-
-            string path = @"F:\vano\Учеба\Универ\Prog\Serpinski\Serpinski\Serp" + size + ".off";
-            //string path = @"C:\Users\RoGad\OneDrive\Рабочий стол\pyramid_of_Serpinsky-main\Serpinski\Serp" + size + ".off";
 
             if (!File.Exists(path))
             {
@@ -49,38 +42,34 @@ namespace Serpinski
                 // координаты всех точек
                 pirPoz(pozition[0], pozition[1], pozition[2], pozition[3], pozition[4], size, path, face);
                 // грани
-                for (int i = 0; i < faces; i++)
+                //for (int i = 0; i < faces; i++)
+                //{
+                //    File.AppendAllText(path, face.faces[i]);
+                //    Console.WriteLine(face.faces[i]);
+                //}
+
+                foreach (string str in face.faces)
                 {
-                    File.AppendAllText(path, face.faces[i]);
-                    Console.WriteLine(face.faces[i]);
+                    File.AppendAllText(path, str);
+                    Console.WriteLine(str);
                 }
                 Console.WriteLine(face.verticeCol);
-                Console.WriteLine(face.facesCol);
-
             }
-
         }
 
         class Faces
         {
             public int facesCol = 0; // Счетчик для полей
             public int verticeCol = 0; // Счетчик для вершин
-            public string[] faces = new string[1000000];
+            public List<string> faces = new List<string>();
             public Faces()
             {
 
-            }
-            public int upFacesCol(int n)
-            {
-                this.facesCol += n;
-                return this.facesCol - n;
             }
             public void upVerticeCol(int n)
             {
                 this.verticeCol += n;
             }
-
-
         }
 
         static void pirPoz(double[] A, double[] B, double[] C, double[] D, double[] S, int size, string path, Faces face)
@@ -95,9 +84,6 @@ namespace Serpinski
             middles[6] = new double[3]; // as
             middles[7] = new double[3]; // cs
             middles[8] = new double[3]; // ds
-
-            //string[][] middlesForFaces = new string[5][]; // Вершины для полей
-
 
             if (size != 0)
             {
@@ -114,14 +100,6 @@ namespace Serpinski
                     middles[8][i] = (D[i] + S[i]) / 2.0; // середина D и S
                 }
 
-                //for (int i = 0; i < 9; i++) {
-                //    string str = middles[i][0].ToString() + " " + middles[i][1].ToString() + " " + middles[i][2].ToString();
-                //    File.AppendAllText(path, str);
-                //}
-
-                //face.faces[face.facesCol++] = "4 " + 
-
-
                 // Ищем координаты вершин таких же 5 пирамидок внутри одной
                 pirPoz(A, middles[0], middles[2], middles[3], middles[6], size - 1, path, face);
                 pirPoz(middles[0], B, middles[1], middles[2], middles[5], size - 1, path, face);
@@ -137,22 +115,17 @@ namespace Serpinski
                 File.AppendAllText(path, C[0].ToString() + " " + C[1].ToString() + " " + C[2].ToString() + "\n");
                 File.AppendAllText(path, D[0].ToString() + " " + D[1].ToString() + " " + D[2].ToString() + "\n");
                 File.AppendAllText(path, S[0].ToString() + " " + S[1].ToString() + " " + S[2].ToString() + "\n");
-                // при одной итерации получается 25, но больше чем 14, тк точки повторяются. исправить
 
                 // записываем грани и индексы вершин в массив
                 int a = 0 + face.verticeCol, b = 1 + face.verticeCol, c = 2 + face.verticeCol, d = 3 + face.verticeCol, s = 4 + face.verticeCol;
-                face.faces[face.upFacesCol(1)] = "4 " + a.ToString() + " " + b.ToString() + " " + c.ToString() + " " + d.ToString() + "\n";
-                face.faces[face.upFacesCol(1)] = "3 " + a.ToString() + " " + s.ToString() + " " + b.ToString() + "\n";
-                face.faces[face.upFacesCol(1)] = "3 " + a.ToString() + " " + s.ToString() + " " + d.ToString() + "\n";
-                face.faces[face.upFacesCol(1)] = "3 " + b.ToString() + " " + s.ToString() + " " + c.ToString() + "\n";
-                face.faces[face.upFacesCol(1)] = "3 " + c.ToString() + " " + s.ToString() + " " + d.ToString() + "\n";
+                face.faces.Add("4 " + a.ToString() + " " + b.ToString() + " " + c.ToString() + " " + d.ToString() + "\n");
+                face.faces.Add("3 " + a.ToString() + " " + s.ToString() + " " + b.ToString() + "\n");
+                face.faces.Add("3 " + a.ToString() + " " + s.ToString() + " " + d.ToString() + "\n");
+                face.faces.Add("3 " + b.ToString() + " " + s.ToString() + " " + c.ToString() + "\n");
+                face.faces.Add("3 " + c.ToString() + " " + s.ToString() + " " + d.ToString() + "\n");
 
                 face.upVerticeCol(5); // Увеличиваем счетчик для последующих записей
             }
-
-
         }
     }
-
-
 }
